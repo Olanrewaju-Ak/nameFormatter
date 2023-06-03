91ea64b000dcf3e4243dd9adf0a91cc2e9a30ed8
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-02 20:21:00 -0700

    name formatter base code

---
+++ test/nameInverterTest.js
@@ -0,0 +1,8 @@
+const chai = require("chai");
+const assert = chai.assert;
+
+const nameInverter = require("../nameInverter");
+
+describe("nameInverter", function () {
+  it("should rearrange a name to last name first", function () {});
+});
