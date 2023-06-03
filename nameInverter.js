65fc66c32593d457e7bc9294304ddd8cf83c5dec
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-02 20:55:29 -0700

    first 3 tests have passed

--- nameInverter.js
+++ nameInverter.js
@@ -1,5 +1,13 @@
-const nameInverter = function(name){
+const nameInverter = function (name) {
+  if (name === "") {
+    return "";
+  }
+  if (name[0] === " " || name[name.length - 1] === " ") {
+    let trimmedName = name.trim();
+    return trimmedName;
+  }
 
+  return name;
 };
 
-module.exports= nameInverter;
+module.exports = nameInverter;
--- test/nameInverterTest.js
+++ test/nameInverterTest.js
@@ -4,5 +4,22 @@ const assert = chai.assert;
 const nameInverter = require("../nameInverter");
 
 describe("nameInverter", function () {
-  it("should rearrange a name to last name first", function () {});
+  it("should return an empty string when passed an empty string", function () {
+    const inputName = "";
+
+    const expectedOutput = "";
+
+    assert.equal(nameInverter(inputName), expectedOutput);
+  });
+
+  it("should return a single name when passed a single name input", function () {
+    const inputName = "name";
+    const expectedOutput = "name";
+    assert.equal(nameInverter(inputName), expectedOutput);
+  });
+  it("should return a single name when passed a single name with extra spaces", function () {
+    const inputName = " name ";
+    const expectedOutput = "name";
+    assert.equal(nameInverter(inputName), expectedOutput);
+  });
 });
